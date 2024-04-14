229f653f04714315a385c9864014bac2e15dc67f
Windsor Nguyen <mn4560@princeton.edu>
2024-04-14 00:48:31 -0400

    Increased max capacity to 350.

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const Quacker = () => {
   const [count, setCount] = useState<number>(0);    // Current count in Quad.
   const [loading, setLoading] = useState(true);     // Loading animation state.
   const [open, setOpen] = useState<boolean>(false); // Confirmation dialog state.
-  const MAX_CAPACITY: number = 250;                 // Maximum capacity of Quad.
+  const MAX_CAPACITY: number = 350;                 // Maximum capacity of Quad.
 
   const capacityMeterStyle = {
     width: `${(count / MAX_CAPACITY) * 100}%`,
